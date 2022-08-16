74e53c20cd4e5d2da0741f7865b78999ce9160e6
Yaseen <yaseen@MacBook-Pro.local>
2022-08-16 17:49:58 +0200

    update

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -8,38 +8,44 @@ import 'definition_screen.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
   static List<Widget> imagesWidgets = [
-Container(
-  margin: EdgeInsets.all(5.0),
-  child: ClipRRect(
-      borderRadius: BorderRadius.all(Radius.circular(5.0)),
-      child: 
-          Image.asset('assets/s2.jpeg', fit: BoxFit.cover, width: 200.0),
+    Container(
+      //  margin: EdgeInsets.all(5.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        child: Image.asset(
+          'assets/s1.jpeg', fit: BoxFit.cover, //width: 200.0
+        ),
       ),
-),
-Container(
-  margin: EdgeInsets.all(5.0),
-  child: ClipRRect(
-      borderRadius: BorderRadius.all(Radius.circular(5.0)),
-      child: 
-          Image.asset('assets/s2.jpeg', fit: BoxFit.cover, width: 200.0),
+    ),
+    Container(
+      //margin: EdgeInsets.all(5.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        child: Image.asset(
+          'assets/s2.jpeg', fit: BoxFit.cover, // width: 200.0,
+        ),
       ),
-),
-Container(
-  margin: EdgeInsets.all(5.0),
-  child: ClipRRect(
-      borderRadius: BorderRadius.all(Radius.circular(5.0)),
-      child: 
-          Image.asset('assets/s2.jpeg', fit: BoxFit.cover, width: 200.0),
+    ),
+    Container(
+      // margin: EdgeInsets.all(5.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        child: Image.asset(
+          'assets/s3.jpeg', fit: BoxFit.cover,
+          //width: 200.0,
+        ),
       ),
-),
-Container(
-  margin: EdgeInsets.all(5.0),
-  child: ClipRRect(
-      borderRadius: BorderRadius.all(Radius.circular(5.0)),
-      child: 
-          Image.asset('assets/s2.jpeg', fit: BoxFit.cover, width: 200.0),
+    ),
+    Container(
+      //margin: EdgeInsets.all(5.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        child: Image.asset(
+          'assets/s4.jpeg', fit: BoxFit.cover,
+          //width: 200.0,
+        ),
       ),
-),
+    ),
   ];
 
   @override
@@ -51,18 +57,19 @@ Container(
             HomeHeader(),
             SizedBox(height: 20),
             CarouselSlider(
-              items: imagesWidgets, 
-            options: CarouselOptions(
-          aspectRatio: 2.0,
-          enlargeCenterPage: true,
-          scrollDirection: Axis.vertical,
-          autoPlay: true,
-        ),),
+              items: imagesWidgets,
+              options: CarouselOptions(
+                aspectRatio: 2.0,
+                enlargeCenterPage: true,
+                scrollDirection: Axis.horizontal,
+                autoPlay: true,
+              ),
+            ),
             Expanded(
               child: GridView.count(
                 crossAxisCount: 2,
-                crossAxisSpacing: 4.0,
-                mainAxisSpacing: 8.0,
+                crossAxisSpacing: 2.0,
+                mainAxisSpacing: 4.0,
                 children: [
                   Center(
                     child: NajmCard(
